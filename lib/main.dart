c299ae156da7bce4710fa692a80d1a212abcab4f
Ahmed-Mohamed-Fahmey <am8844168@gmail.com>
2024-08-29 09:09:31 +0300

    step-3

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:inote/firebase_options.dart';
 import 'package:inote/views/login_view.dart';
 import 'package:inote/views/register_view.dart';
 import 'package:inote/views/verify_email_view.dart';
+import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -39,8 +40,7 @@ class HomePage extends StatelessWidget {
             final user = FirebaseAuth.instance.currentUser;
             if (user != null) {
               if (user.emailVerified) {
-                print("email is verified");
-                return const Scaffold(body: Center(child: Text('Done')));
+                return const NotesView();
               } else {
                 return const VerifyEmailView();
               }
@@ -54,4 +54,122 @@ class HomePage extends StatelessWidget {
       },
     );
   }
+}
+
+enum MenuAction { logout, settings }
+
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main UI'),
+        actions: [
+          PopupMenuButton<MenuAction>(
+            onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  devtools.log(value.toString());
+                  final shouldLogOut = await showLogoutDialog(context);
+                  if (shouldLogOut) {
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      '/login/',
+                      (_) => false,
+                    );
+                  }
+                  devtools.log(shouldLogOut.toString());
+                  break;
+                case MenuAction.settings:
+                  devtools.log(value.toString());
+                  final shouldOpenSettings = await showSettingsDialog(context);
+                  devtools.log(shouldOpenSettings.toString());
+                  if (shouldOpenSettings) {
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      '/login/',
+                      (_) => false,
+                    );
+                    devtools
+                        .log('xD I tricked there is no settings you goose!');
+                  }
+                  break;
+                default:
+                  break;
+              }
+            },
+            itemBuilder: (context) {
+              return const [
+                PopupMenuItem<MenuAction>(
+                  value: MenuAction.logout,
+                  child: Text('log out'),
+                ),
+                PopupMenuItem<MenuAction>(
+                  value: MenuAction.settings,
+                  child: Text('settings'),
+                ),
+              ];
+            },
+          )
+        ],
+      ),
+      body: const Text('Hello World'),
+    );
+  }
+}
+
+Future<bool> showLogoutDialog(context) {
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Log out'),
+        content: const Text('Are you sure you want to log out?'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(false);
+            },
+            child: const Text('cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop(true);
+            },
+            child: const Text('Log out'),
+          ),
+        ],
+      );
+    },
+  ).then((value) => (value) ?? false);
+}
+
+Future<bool> showSettingsDialog(context) {
+  return showDialog<bool>(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: const Text('Settings'),
+      content: const Text('Are you sure you want to open settings?'),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(false);
+          },
+          child: const Text('cancel'),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(true);
+          },
+          child: const Text('Open settings'),
+        )
+      ],
+    ),
+  ).then((value) => (value) ?? false);
 }
